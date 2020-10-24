9eb4ce4db332a4a8cba9d841c517df4c577a3af8
Rick Sousa <rickssti@outlook.com>
2020-10-24 19:55:19 -0300

    Updates

---
+++ precache-manifest.2731f63401b2c3bc7964532edf2e18cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa08a0f01df8c2fcf3773aa938cd93b",
+    "url": "/Project-movie-card-library-stateful/index.html"
+  },
+  {
+    "revision": "a729fc308366d3af1ec3",
+    "url": "/Project-movie-card-library-stateful/static/css/main.5d58c8a2.chunk.css"
+  },
+  {
+    "revision": "212cdba34f355b1652cf",
+    "url": "/Project-movie-card-library-stateful/static/js/2.a924ad4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Project-movie-card-library-stateful/static/js/2.a924ad4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a729fc308366d3af1ec3",
+    "url": "/Project-movie-card-library-stateful/static/js/main.7bf2423d.chunk.js"
+  },
+  {
+    "revision": "00554ba5df104c2eba16",
+    "url": "/Project-movie-card-library-stateful/static/js/runtime-main.1479d67e.js"
+  }
+]);
